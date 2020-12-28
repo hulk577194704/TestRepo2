b1fb45892347e18727068a984f5756b987a05d9c
zs <zs@qq.com>
2020-12-28 17:04:33 +0800

    开发签到-100%

--- Sigin.java
+++
@@ -1,6 +0,0 @@
-public class Signin{
-	private String name;
-	public void setName(String name){
-		this.name = name;
-	}
-}
---
+++ Signin.java
@@ -0,0 +1,6 @@
+public class Signin{
+	private String name;
+        public static void main(String[] args){
+                System.out.prinln("Singin.java");
+        }
+}
